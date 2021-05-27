fd65c1e7b59ba4b0a1cca6ef2f4fe7fa944f1ff8
Ryan Els <39189847+RyanGuitar@users.noreply.github.com>
2021-05-27 09:42:19 +0200

    Delete slider directory

--- slider/main.js
+++
@@ -1,6 +0,0 @@
-import Slider from './includes/slider.js'
-
-document.getElementById('slider').innerHTML = Slider;
-document.getElementById('slider2').innerHTML = Slider;
-document.getElementById('slider3').innerHTML = Slider;
-document.getElementById('slider4').innerHTML = Slider;
