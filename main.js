78fed1def6135330f3f508e06503a0786941ccb9
Ryan Els <39189847+RyanGuitar@users.noreply.github.com>
2021-05-27 09:43:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import Slider from './includes/slider.js'
+
+document.getElementById('slider').innerHTML = Slider;
+document.getElementById('slider2').innerHTML = Slider;
+document.getElementById('slider3').innerHTML = Slider;
+document.getElementById('slider4').innerHTML = Slider;
